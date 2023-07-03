e13f32faecfd0b0a6b71e7d9e7cbc2beec3a25d3
weriomat <59094810+weriomat@users.noreply.github.com>
2023-07-03 15:05:34 +0200

    Update null-ls.lua

--- plugins/null-ls.lua
+++ plugins/null-ls.lua
@@ -1,20 +1,15 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  opts = function(_, opts)
-    -- add more things to the ensure_installed table protecting against community packs modifying it
-    opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-      "lua",
-      "c",
-      -- "bash",
-      -- "cmake",
-      -- "comment",
-      -- "json",
-      -- "java",
-      -- "nix",
-      "python",
-      -- "rust",
-      -- "yaml",
-      -- "llvm",
-    })
-  end,
-}
+  "jose-elias-alvarez/null-ls.nvim",
+  opts = function(_, config)
+    -- config variable is the default configuration table for the setup function call
+    local null_ls = require "null-ls"
+
+    -- Check supported formatters and linters
+    -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
+    -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
+    config.sources = {
+      -- Set a formatter
+      null_ls.builtins.formatting.stylua,
+      null_ls.builtins.formatting.prettier,
+    }
+    return config -- return final config table
