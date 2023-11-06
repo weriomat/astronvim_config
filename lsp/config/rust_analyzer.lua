b63767d2ad9dad4807924c68334b47cb21faa3f4
Elias <e.engel@campus.tu-berlin.de>
2023-11-06 13:46:58 +0100

    rewritten config, added support for c, python and rust

---
+++ lsp/config/rust_analyzer.lua
@@ -0,0 +1,11 @@
+return {
+  -- support for rust ig
+  settings = {
+    ["rust-analyzer"] = {
+      cargo = {
+        extraEnv = { CARGO_PROFILE_RUST_ANALYZER_INHERITS = "dev" },
+        extraArgs = { "--profile", "rust-analyzer" },
+      },
+    },
+  },
+}
---
+++ lsp/formatting.lua
@@ -0,0 +1,9 @@
+return {
+  format_on_save = {
+    enabled = true,
+  },
+  timeout_ms = 1000, -- default format timeout
+  -- filter = function(client) -- fully override the default formatting function
+  --   return true
+  -- end
+}
